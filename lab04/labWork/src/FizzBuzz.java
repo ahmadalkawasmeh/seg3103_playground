a8796606857273b1f34f9fa2cd701e6bb144252c
ahmadalkawasmeh <ahmadalkawasmeh@cmail.carleton.ca>
2024-06-12 21:21:34 -0400

    commit 2
    Green

--- lab04/labWork/src/FizzBuzz.java
+++ lab04/labWork/src/FizzBuzz.java
@@ -1,4 +1,14 @@
 public class FizzBuzz {
 
+    private String result;
+    public FizzBuzz() {
+        this.result = "";
+    }
 
+    public String check(int num) {
+        if (num % 3 == 0){
+            result = "Fizz";
+        }
+        return result;
+    }
 }
