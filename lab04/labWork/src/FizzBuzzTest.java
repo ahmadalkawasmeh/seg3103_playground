f6d2bbb8f605e3411ceff75aacbdd023fcec17ee
ahmadalkawasmeh <ahmadalkawasmeh@cmail.carleton.ca>
2024-06-12 20:46:10 -0400

    commit1
    Setting up project

---
+++ lab04/labWork/src/FizzBuzzTest.java
@@ -0,0 +1,38 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class FizzBuzzTest {
+    @BeforeEach
+    void setUp(){
+        FizzBuzz fiz = new FizzBuzz();
+        int t1 = 3;
+        int t2 = 5;
+        int t3 = 15;
+
+
+    }
+
+    @AfterEach
+    void tearDown(){
+
+
+    }
+
+    @Test
+    void fizz_test(){
+
+
+    }
+
+    @Test
+    void buzz_test(){
+
+    }
+
+    @Test
+    void fizz_buzz_test(){
+
+    }
+
+}
