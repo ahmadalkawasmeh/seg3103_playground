b88bdd82498c6c2dff61667a0287da406efd34e7
ahmadalkawasmeh <ahmadalkawasmeh@cmail.carleton.ca>
2024-06-12 22:10:09 -0400

    commit 11
    RED

--- lab04/labWork/src/FizzBuzzTest.java
+++ lab04/labWork/src/FizzBuzzTest.java
@@ -7,6 +7,7 @@ public class FizzBuzzTest {
     private int t1;
     private int t2;
     private int t3;
+    private int t4;
     private FizzBuzz fiz;
     private String result;
 
@@ -18,6 +19,7 @@ public class FizzBuzzTest {
         this.t1 = 3;
         this.t2 = 5;
         this.t3 = 15;
+        this.t4 = 14;
     }
 
     @AfterEach
@@ -41,7 +43,14 @@ public class FizzBuzzTest {
     void fizz_buzz_test(){
         result = fiz.check(t3);
         assert(result == "FizzBuzz");
-
     }
+
+    @Test
+    void not_dividable(){
+        result = fiz.check(t4);
+        assert(result == String.valueOf(t4));
+    }
+
+
 
 }
