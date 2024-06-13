a25e035e2fb9a0e8f979f1201f2764519269558c
ahmadalkawasmeh <ahmadalkawasmeh@cmail.carleton.ca>
2024-06-12 22:04:16 -0400

    commit 9
    GREEN

--- lab04/labWork/src/FizzBuzz.java
+++ lab04/labWork/src/FizzBuzz.java
@@ -7,16 +7,24 @@ public class FizzBuzz {
     }
 
     public String check(int num) {
+        if (num % 3 == 0 && num % 5 == 0){
+            result = "FizzBuzz";
+            return result;
+        }
+
         if (num % 3 == 0){
             result = "Fizz";
+            return result;
         }
 
         if (num % 5 == 0){
             result = "Buzz";
+            return result;
         }
 
         if (result.isEmpty()){
             result = String.valueOf(num);
+            return result;
         }
 
         return result;
