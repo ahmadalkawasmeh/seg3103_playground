f3f3b0f52ce6b70deeb00d12933e48be59db287c
ahmadalkawasmeh <ahmadalkawasmeh@cmail.carleton.ca>
2024-06-12 21:13:55 -0400

    commit1
    RED

--- lab04/labWork/src/FizzBuzzTest.java
+++ lab04/labWork/src/FizzBuzzTest.java
@@ -2,25 +2,37 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+
 public class FizzBuzzTest {
+    private int t1;
+    private int t2;
+    private int t3;
+    private FizzBuzz fiz;
+    private int resultNum;
+    private String resultStr;
+
+
     @BeforeEach
     void setUp(){
-        FizzBuzz fiz = new FizzBuzz();
-        int t1 = 3;
-        int t2 = 5;
-        int t3 = 15;
-
-
+        this.fiz = new FizzBuzz();
+        this.resultNum = 0;
+        this.resultStr = "";
+        this.t1 = 3;
+        this.t2 = 5;
+        this.t3 = 15;
     }
 
     @AfterEach
     void tearDown(){
 
-
     }
 
     @Test
     void fizz_test(){
+        resultStr = fiz.check(t1);
+        assert(resultStr == "Fizz");
+
+
 
 
     }
